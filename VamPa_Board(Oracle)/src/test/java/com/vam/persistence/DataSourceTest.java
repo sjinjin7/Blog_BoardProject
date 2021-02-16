cf4f4d9d16a2004510bcf7324a92c4c5b16b6b3e
SeoJin Kim <sjinjin7@gmail.com>
2021-02-16 19:05:06 +0900

    [STS][스프링 게시판][0] MVC 프로젝트 생성 및 기본설정
    https://kimvampa.tistory.com/158?category=843151
    https://kimvampa.tistory.com/159?category=843151
    https://kimvampa.tistory.com/162?category=843151

---
+++ VamPa_Board(Oracle)/src/test/java/com/vam/persistence/DataSourceTest.java
@@ -0,0 +1,46 @@
+package com.vam.persistence;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class DataSourceTest {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired private 
+	SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testConnection() {
+		
+		try(
+				Connection con = dataSource.getConnection();
+				SqlSession session = sqlSessionFactory.openSession();
+			
+			){
+			
+				System.out.println("con = " + con);
+				System.out.println("session = " + session);
+			
+			} catch(Exception e) {
+			
+				e.printStackTrace();
+			
+			}
+		
+	}
+	
+}
+ 
